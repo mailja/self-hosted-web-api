090baf5135c4d76181056e4d6412930d4f2ac265
Marko <markus.iljas@gmail.com>
2017-06-01 00:23:21 +0200

    Add project files.

---
+++ SelfHostedWebApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostedWebApi
+{
+  class Program
+  {
+    static void Main(string[] args) {
+      WebApp.Start<Startup>("http://localhost:5000/");
+      Console.ReadLine();
+    }
+  }
+}
